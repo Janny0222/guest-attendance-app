cf179ca1793671d2746d37c732611b17edc8ec14
Janny0222 <villarueljanmichael@gmail.com>
2025-09-03 14:18:39 +0800

    test-prod

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ io.on("connection", (socket) => {
 app.set("socketio", io);
 
 
-server.listen(PORT, "0.0.0.0", () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
