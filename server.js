0f8ded1d8fff3c6070e9788462f060576ae0d90a
Janny0222 <villarueljanmichael@gmail.com>
2025-09-04 07:33:48 +0800

    add port

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ io.on("connection", (socket) => {
 app.set("socketio", io);
 
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
